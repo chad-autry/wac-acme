420b63ccece255a501817b0684fb950f31a8003a
Chad Autry <chad.autry@gmail.com>
2016-09-26 07:47:39 -0500

    Update pushAcmeResponseToEtcd.sh
    etcd v3 uses put vs set

--- pushAcmeResponseToEtcd.sh
+++ pushAcmeResponseToEtcd.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 # Push the response, Domain Token expected as the second param, Account THumbprint as the 3rd. (Domain as the first, but not used)
-/usr/bin/etcdctl set /acme "$2.$3"
+/usr/bin/etcdctl put /acme "$2.$3"
 
 # Sleep 30 seconds so the webservers have a chance to react
 sleep 30
