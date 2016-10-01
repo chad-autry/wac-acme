e143381f71cc077316735830305420a5e534ae7f
Chad Autry <chad.autry@gmail.com>
2016-10-01 09:23:08 -0500

    Update pushAcmeResponseToEtcd.sh
    Add domain

--- pushAcmeResponseToEtcd.sh
+++ pushAcmeResponseToEtcd.sh
@@ -1,8 +1,9 @@
 #!/bin/sh
 
-# Push the response, Domain Token expected as the third param, Account Thumbprint as the fourth. (Domain as the second, but not used)
+# Push the response, Domain expected as the second param, Domain Token expected as the third param, Account Thumbprint as the fourth.
 if [$1 == 'install']
     then
+        /usr/bin/etcdctl set /acme/domain $2
         /usr/bin/etcdctl set /acme/token $3
         /usr/bin/etcdctl set /acme/thumbprint $4
         # I don't want to bother splitting values on the other side, so just set an independent watched value
