993a2335f4589b08fc1df22e0600b544e319e43f
Chad Autry <chad.autry@gmail.com>
2016-10-01 10:10:47 -0500

    Update pushAcmeResponseToEtcd.sh
    Still fixing the comparison

--- pushAcmeResponseToEtcd.sh
+++ pushAcmeResponseToEtcd.sh
@@ -1,16 +1,15 @@
 #!/bin/sh
 
 # Push the response, Domain expected as the second param, Domain Token expected as the third param, Account Thumbprint as the fourth.
-if ["$1" -eq "install"]
-    then
-        /usr/bin/etcdctl set /acme/domain $2
-        /usr/bin/etcdctl set /acme/token $3
-        /usr/bin/etcdctl set /acme/thumbprint $4
-        # I don't want to bother splitting values on the other side, so just set an independent watched value
-        /usr/bin/etcdctl set /acme/watched date +%s%N
+if [ "$1" = "install" ]; then
+    /usr/bin/etcdctl set /acme/domain $2
+    /usr/bin/etcdctl set /acme/token $3
+    /usr/bin/etcdctl set /acme/thumbprint $4
+    # I don't want to bother splitting values on the other side, so just set an independent watched value
+    /usr/bin/etcdctl set /acme/watched date +%s%N
 
-        # Sleep 30 seconds so the webservers have a chance to react
-        sleep 30
-fi
+    # Sleep 30 seconds so the webservers have a chance to react
+    sleep 30
+fi;
 
 
