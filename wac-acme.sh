f7316823893405be12a7af92890024074421cb63
Chad Autry <chad.autry@gmail.com>
2016-10-06 20:15:26 -0500

    Update wac-acme.sh

--- wac-acme.sh
+++ wac-acme.sh
@@ -12,8 +12,9 @@ umask 0022
 # Request the certificate be signed, domain comes as the second parameter
 /usr/bin/letsencrypt.sh sign -P /usr/bin/pushAcmeResponseToEtcd.sh -a account.key -k server.key -c server.pem $2
 
-/usr/bin/etcdctl set /ssl/key "\\$(cat server.key)"
-/usr/bin/etcdctl set /ssl/server_pem "\\$(cat server.pem)"
-/usr/bin/etcdctl set /ssl/server_chain "\\$(cat server.pem_chain)"
+# -- Means there are no more command options, needed since the cert files start with '-'
+/usr/bin/etcdctl set -- /ssl/key "$(cat server.key)"
+/usr/bin/etcdctl set -- /ssl/server_pem "$(cat server.pem)"
+/usr/bin/etcdctl set -- /ssl/server_chain "$(cat server.pem_chain)"
 # Set a single watch value to be updated when all the certs are updated
 /usr/bin/etcdctl set /ssl/watched "$(date +%s%N)"
