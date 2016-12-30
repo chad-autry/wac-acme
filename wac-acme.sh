6fbf6c862ad34a4400a88b7b4c07a47ea913a530
Chad Autry <chad.autry@gmail.com>
2016-12-30 08:00:13 -0600

    Update wac-acme.sh
    Get domain name and e-mail from etcd, instead of script parameters

--- wac-acme.sh
+++ wac-acme.sh
@@ -6,11 +6,15 @@ openssl genrsa -out account.key 4096
 openssl genrsa -out server.key 4096
 umask 0022
 
-# Register the key with letsencrypt, e-mail comes as the first parameter
-/usr/bin/letsencrypt.sh register -a account.key -e $1
+# Get the email and domain name from etcd
+email=$(/usr/bin/etcdctl get /domain/email)
+domain=$(/usr/bin/etcdctl get /domain/name)
 
-# Request the certificate be signed, domain comes as the second parameter
-/usr/bin/letsencrypt.sh sign -P /usr/bin/pushAcmeResponseToEtcd.sh -a account.key -k server.key -c server.pem $2
+# Register the key with letsencrypt
+/usr/bin/letsencrypt.sh register -a account.key -e $email
+
+# Request the certificate be signed
+/usr/bin/letsencrypt.sh sign -P /usr/bin/pushAcmeResponseToEtcd.sh -a account.key -k server.key -c server.pem $domain
 
 # -- Means there are no more command options, needed since the cert files start with '-'
 /usr/bin/etcdctl set -- /ssl/key "$(cat server.key)"
